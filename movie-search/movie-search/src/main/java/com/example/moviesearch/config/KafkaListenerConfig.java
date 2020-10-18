027b90cbfbbce01b89de0caa9b54338806fa6939
deena11 <35695739+deena11@users.noreply.github.com>
2020-10-19 01:24:40 +0530

    Update KafkaListenerConfig.java

--- movie-search/movie-search/src/main/java/com/example/moviesearch/config/KafkaListenerConfig.java
+++ movie-search/movie-search/src/main/java/com/example/moviesearch/config/KafkaListenerConfig.java
@@ -11,17 +11,25 @@ import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.beans.factory.annotation.Value;
 
 @EnableKafka
 @Configuration
 public class KafkaListenerConfig {
+	
+	@Value(value = "${kafka.bootstrapAddress}")
+	private String bootstrapAddress;
+
+	@Value(value = "${kafka.consumer.group.id}")
+	private String groupId;
+
 
     @Bean
     public ConsumerFactory<String, String> consumerFactory() {
         Map<String, Object> config = new HashMap<>();
 
-        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
-        config.put(ConsumerConfig.GROUP_ID_CONFIG, "msg");
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+        config.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
         config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
 
