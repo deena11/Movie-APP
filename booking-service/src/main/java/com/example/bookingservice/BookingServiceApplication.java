0e7ca7f66808e4bb32acbdc158869bee81dc92f9
deena11 <35695739+deena11@users.noreply.github.com>
2020-10-19 01:12:00 +0530

    update booking application

--- booking-service/src/main/java/com/example/bookingservice/BookingServiceApplication.java
+++ booking-service/src/main/java/com/example/bookingservice/BookingServiceApplication.java
@@ -14,6 +14,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 @EnableResourceServer
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @EnableEurekaClient
+@EnableHystrix
 public class BookingServiceApplication {
 	
 	@Bean
